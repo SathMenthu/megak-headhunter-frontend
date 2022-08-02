a797b101bb61223884537a115d2dc1479a6cb655
Wiktor Kuna <you@example.com>
2022-08-02 21:26:41 +0200

    changes: tailwind.config and .eslintignore

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        HRwhite: '#F7F7F7',
+        HRblack: {
+          DEFAULT: '#1E1E1F',
+          100: '#292A2B',
+          200: '#222224',
+        },
+        HRred: {
+          DEFAULT: '#E02735',
+        },
+        HRgrey: {
+          DEFAULT: '#CFCFCF'},
+          light: '#CFCFCF',
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
