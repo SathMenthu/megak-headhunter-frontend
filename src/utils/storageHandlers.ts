b25a47a9ead553c27d0a2417b40e45f64d1af83d
Jakub Król <jakubkrol@o2.pl>
2022-08-21 22:22:32 +0200

    Empty

--- src/utils/storageHandlers.ts
+++
@@ -1,9 +0,0 @@
-export const setStorage = <T>(key: string, value: T): void => {
-  const json = JSON.stringify(value);
-  localStorage.setItem(key, json);
-};
-
-export const getStorage = (key: string) => {
-  const json = localStorage.getItem(key);
-  return json ? JSON.parse(json) : null;
-};
